4c8e2a5c3167372366b3f31b77e6192d7eff3142
angaadabhilash <angadabhilaash@gmail.com>
2022-08-11 13:52:52 +0400

    NotePad-App
    Save Your Most Private Secrets
    Made With: React
    Powered By: localhost3000

---
+++ note-app/src/App.js
@@ -0,0 +1,76 @@
+
+import { useEffect, useState } from 'react';
+import uuid from 'react-uuid';
+import './App.css';
+import Main from './Main';
+import Sidebar from './Sidebar';
+
+function App() {
+  //Use State
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+      );
+
+  const [activeNote, setActiveNote] = useState(false);
+
+    //Use Effects
+    useEffect(() => {
+      localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
+
+    //OnAdd Note
+    const onAddNote = () => {
+      const newNote = {
+        id: uuid(),
+        title: "Untitled Note",
+        body: "",
+        lastModified: Date.now(),
+      };
+
+      setNotes([newNote, ...notes]);
+      setActiveNote(newNote.id);
+
+    };
+
+    //OnDelete Note
+    const onDeleteNote = (noteId) => {
+      setNotes(notes.filter(({ id }) => id !== noteId));
+    }
+
+    //OnUpdate Note
+    const onUpdateNote = (updatedNote) => {
+      const updatedNotesArr = notes.map((note) => {
+        if (note.id === updatedNote.id) {
+          return updatedNote;
+        }
+
+        return note;
+      })
+
+      setNotes(updatedNotesArr);
+    }
+
+
+
+    //GetActiveNote
+    const getActiveNote = () =>{
+      return notes.find(({ id }) => id === activeNote);
+    };
+
+
+  return (
+    <div className="App">
+      <Sidebar
+      notes={notes}
+      onAddNote={onAddNote}
+      onDeleteNote={onDeleteNote}
+      activeNote={activeNote}
+      setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote() } onUpdateNote={onUpdateNote}
+      />
+    </div>
+  );
+}
+
+export default App;
